905a97fbd3976e8f71316db495e0ae6e521cdf0e
jdubbs1325 <jackwhite13@gmail.com>
2022-04-04 03:10:19 -0400

    Update MyApps.sh

--- MyApps.sh
+++ MyApps.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-pacman -S nitrogen htop transmission-gtk mpv alacritty pcmanfm firefox ranger xfce4-power-manager lxappearance rofi neovim
-deadbeef dunst
+pacman -S base-devel nitrogen htop transmission-gtk mpv alacritty pcmanfm firefox ranger xfce4-power-manager lxappearance rofi neovim
+deadbeef dunst &&
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
