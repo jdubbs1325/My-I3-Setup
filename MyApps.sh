77dcd098bad86166c9fcd25cf0791c2a05efdd59
jdubbs1325 <jackwhite13@gmail.com>
2022-04-09 01:21:34 -0400

    Update MyApps.sh

--- MyApps.sh
+++ MyApps.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-pacman -S base-devel nitrogen htop transmission-gtk mpv alacritty pcmanfm firefox ranger xfce4-power-manager lxappearance rofi neovim
-deadbeef dunst polkit-gnome xfce4-power-manager picom
+pacman -S nitrogen htop transmission-gtk mpv alacritty pcmanfm firefox ranger xfce4-power-manager rofi neovim dunst polkit-gnome xfce4-power-manager picom
